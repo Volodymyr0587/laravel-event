66752c3d849b565b5ccee8d54081a19dd715d13d
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-09-28 10:48:35 +0300

    Add links for events and gallery at navigation

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resourse('/events', EventController::class);
+    Route::resource('/events', EventController::class);
     Route::resource('/galleries', GalleryController::class);
 });
 

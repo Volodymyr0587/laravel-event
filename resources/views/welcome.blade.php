cb1c04fbc8613db5ebc31f2093c2471547b5ad3e
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-10-02 20:37:17 +0300

    Show message if no events

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,15 @@
     <!-- component -->
     <section class="bg-white dark:bg-gray-900">
         <div class="container px-6 py-10 mx-auto">
-            <h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">All Events</h1>
+            <h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
+                All Events
+            </h1>
 
             <div class="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2">
-                @foreach ($events as $event)
+                @forelse ($events as $event)
                     <div class="lg:flex">
                         <img class="object-cover w-full h-56 rounded-lg lg:w-64"
-                            src="{{ asset('/storage/' . $event->image) }}"
-                            alt="{{ $event->title }}">
+                            src="{{ asset('/storage/' . $event->image) }}" alt="{{ $event->title }}">
 
                         <div class="flex flex-col justify-between py-6 lg:mx-6">
                             <a href="{{ route('eventShow', $event->id) }}"
@@ -25,7 +26,12 @@
                             </span>
                         </div>
                     </div>
-                @endforeach
+                @empty
+                    <p class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
+                        Unfortunately, there is nothing interesting yet :(
+                        Be sure to come back a little later or create your own event.
+                    </p>
+                @endforelse
             </div>
         </div>
         {{-- pagination  --}}

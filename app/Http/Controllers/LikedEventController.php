1eead29274363a57921e74864cef69fd5ca40cfb
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-10-03 15:33:45 +0300

    __invoke() method for LikedEventController

--- app/Http/Controllers/LikedEventController.php
+++ app/Http/Controllers/LikedEventController.php
@@ -1,16 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Event;
 
-use Illuminate\Http\Request;
 
 class LikedEventController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke()
     {
-        //
+        $events = Event::with('likes')->whereHas('likes', function ($q){
+            $q->where('user_id', auth()->id());
+        })->get();
+
+        return view('likedEvents', compact('events'));
     }
 }
